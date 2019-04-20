fddca15c29c3f29c9224f76ad514e110770264df
Jorge Rojo <jorge.rojodiseno@gmail.com>
2019-04-20 23:03:27 +0200

    redux initial state

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import path from 'path';
 import express from 'express';
 
 import pageTemplate from './pageTemplate';
+import serverRender from './serverRender';
+import initialState from './redux/initialState';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,8 +12,7 @@ app.use('/', express.static(path.resolve(__dirname, '../public')));
 
 app.get('/*', async (req, res) => {
     const response = pageTemplate({
-        content: 'template loaded',
-        state: { hello: 'world'},
+        ...(await serverRender(req, initialState)),
         title: "Speedrun games",
     });
     res.send(response);
---
+++ src/serverRender.js
@@ -0,0 +1,9 @@
+
+const render = async (req, initialState) => { 
+    return {
+        content: `Server render`,
+        state: initialState
+    };
+};
+
+export default render;
