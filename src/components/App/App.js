17466747bfd3a553ac6c730e7141b1e7d72aded9
Jorge Rojo <jorge.rojodiseno@gmail.com>
2019-04-20 23:26:47 +0200

    ssr react component

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="container my-5">
+            App component...
+        </div>
+    );
+};
+
+export default App;
--- src/serverRender.js
+++ src/serverRender.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+import App from './components/App';
 
 const render = async (req, initialState) => { 
+
+    const content = renderToString(<App />);
+
     return {
-        content: `Server render`,
+        content,
         state: initialState
     };
 };
