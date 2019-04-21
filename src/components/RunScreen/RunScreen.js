ed6a86ad82ed1a55c6e64eaecfed7a594d3ac167
Jorge Rojo <jorge.rojodiseno@gmail.com>
2019-04-21 02:42:08 +0200

    video btn

--- src/components/RunScreen/RunScreen.js
+++ src/components/RunScreen/RunScreen.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Title from '../Title';
 import GoBackBtb from '../GoBackBtb';
+import VideoBtn from './VideoBtn';
 
-const RunScreen = ({ run }) => { 
+const RunScreen = ({ run }) => {
+
+    const { id, videoLink } = run;
 
     return (
         <>
-            <GoBackBtb /> 
-            <Title>{run.id}</Title> 
+            <GoBackBtb />
+            {videoLink &&
+                <VideoBtn videoLink={videoLink} />
+            }
+            <Title>{id}</Title>
         </>
     );
 }
